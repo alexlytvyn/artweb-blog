8b06c16d6845b0e88890c35be493bd97a6dce95e
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2019-06-23 16:08:29 +0300

    Add Admin Dashboard.

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,40 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="jumbotron">
+					<p><span class="label label-primary">Категорій: 0</span></p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="jumbotron">
+					<p><span class="label label-primary">Статей: 0</span></p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="jumbotron">
+					<p><span class="label label-primary">Відвідувачів: 0</span></p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="jumbotron">
+					<p><span class="label label-primary">Сьогодні: 0</span></p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6"><a href="" class="list-group-item">
+				<a href="" class="btn btn-block btn-default">Додати категорію</a>
+				<h4 class="list-group-item-heading">Категорія перша</h4>
+				<p class="list-group-item-text">Кількість статей: </p>
+				</a></div>
+			<div class="col-md-6"><a href="" class="list-group-item">
+				<a href="" class="btn btn-block btn-default">Додати статтю</a>
+				<h4 class="list-group-item-heading">Стаття перша</h4>
+				<p class="list-group-item-text">Категорія</p>
+				</a></div>
+		</div>
+	</div>
+@endsection
